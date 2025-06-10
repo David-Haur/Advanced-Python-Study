28c25a249952aa68af237f1b61d7226ac638d966
Wujlchn <wujlchn@163.com>
2025-06-10 20:09:01 +0800

    多重继承

---
+++ 多重继承.py
@@ -0,0 +1,32 @@
+class Animal(object):
+    pass
+
+class RunnableMixIn(object):
+    def run(self):
+        print('Running...')
+
+class FlyableMixIn(object):
+    def fly(self):
+        print('Flying...')
+
+# 大类:
+class Mammal(Animal):
+    pass
+
+class Bird(Animal):
+    pass
+
+# 各种动物:
+class Dog(Mammal, RunnableMixIn):
+    pass
+
+class Bat(Mammal, FlyableMixIn):
+    pass
+
+class Parrot(Bird):
+    pass
+
+class Ostrich(Bird):
+    pass
+
+
